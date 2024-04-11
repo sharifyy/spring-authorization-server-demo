e840551cc5c624ec32377e565645531aa16a32d8
Mohamad <sharifyy@gmail.com>
2024-04-11 15:13:11 +0330

    token customization

--- src/main/java/com/sharifyy/authserverdemo/SecurityConfig.java
+++ src/main/java/com/sharifyy/authserverdemo/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
@@ -36,5 +38,13 @@ public class SecurityConfig {
 			.formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.loginPage("/login"));
 
 		return httpSecurity.build();
+	}
+
+	@Bean
+	public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer(){
+		return context -> {
+			context.getClaims().claim("claim_demonstration","whatever");
+			context.getClaims().claim("roles",context.getPrincipal().getAuthorities().stream().map(Object::toString).toList());
+		};
 	}
 }
