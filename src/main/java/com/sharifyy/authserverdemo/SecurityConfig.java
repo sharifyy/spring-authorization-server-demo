678a69ddfd4610fe7c493f85d956b235dc2d9262
Mohamad <sharifyy@gmail.com>
2024-04-11 18:03:21 +0330

    opaque token

--- src/main/java/com/sharifyy/authserverdemo/SecurityConfig.java
+++ src/main/java/com/sharifyy/authserverdemo/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.oauth2.server.authorization.client.Registere
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
@@ -76,6 +77,7 @@ public class SecurityConfig {
 				.build()
 			)
 			.tokenSettings(TokenSettings.builder()
+				.accessTokenFormat(OAuth2TokenFormat.REFERENCE)
 				.accessTokenTimeToLive(Duration.ofMinutes(5))
 				.refreshTokenTimeToLive(Duration.ofMinutes(15))
 				.authorizationCodeTimeToLive(Duration.ofSeconds(60))
