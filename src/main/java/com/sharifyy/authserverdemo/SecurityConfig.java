a96d3d9d3a24e77ee47d5fe66ce01a51ec17a469
Mohamad <sharifyy@gmail.com>
2024-04-11 16:29:12 +0330

    user, client in memory implementation and remove config in yml

--- src/main/java/com/sharifyy/authserverdemo/SecurityConfig.java
+++ src/main/java/com/sharifyy/authserverdemo/SecurityConfig.java
@@ -5,12 +5,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+import java.util.UUID;
 
 @Configuration
 public class SecurityConfig {
@@ -33,18 +48,51 @@ public class SecurityConfig {
 	@Order(2)
 	public SecurityFilterChain defaultFilterChain(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.authorizeHttpRequests(requestMatcherRegistry ->
-			requestMatcherRegistry.requestMatchers("/login", "/error","/logo.png").permitAll()
-				.anyRequest().authenticated())
+				requestMatcherRegistry.requestMatchers("/login", "/error", "/logo.png").permitAll()
+					.anyRequest().authenticated())
 			.formLogin(httpSecurityFormLoginConfigurer -> httpSecurityFormLoginConfigurer.loginPage("/login"));
 
 		return httpSecurity.build();
 	}
 
+
 	@Bean
-	public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer(){
+	public RegisteredClientRepository registeredClientRepository() {
+		RegisteredClient client1 = RegisteredClient.withId(UUID.randomUUID().toString())
+			.clientId("javatalks-web-app")
+			.clientSecret("secret")
+			.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+			.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+			.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+			.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+			.redirectUri("http://127.0.0.1:8080/login/oauth2/code/java-talks")
+			.scope(OidcScopes.OPENID)
+			.scope(OidcScopes.EMAIL)
+			.clientSettings(ClientSettings.builder()
+				.requireAuthorizationConsent(true)
+				.build()
+			)
+			.build();
+
+		return new InMemoryRegisteredClientRepository(client1);
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService(){
+		UserDetails user1 = User.withUsername("sharifyy").password("secret").authorities("developer").build();
+		return new InMemoryUserDetailsManager(user1);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+	@Bean
+	public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer() {
 		return context -> {
-			context.getClaims().claim("claim_demonstration","whatever");
-			context.getClaims().claim("roles",context.getPrincipal().getAuthorities().stream().map(Object::toString).toList());
+			context.getClaims().claim("claim_demonstration", "whatever");
+			context.getClaims().claim("roles", context.getPrincipal().getAuthorities().stream().map(Object::toString).toList());
 		};
 	}
 }
